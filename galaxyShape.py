Masking fixed and completed

galaxyShape.py
@@ -6,6 +6,7 @@ import tkinter as tk
 from matplotlib.widgets import Button
 from prompt import call
 
+angle = None
 vertices = []
 ax = plt.subplots
 fig = plt.subplots
@@ -33,7 +34,7 @@ def closer(event):
 
 def process_vertices(x, y):
     global vertices
-    vertices.append([x, y])
+    vertices.append([math.floor(x), math.floor(y)])
     [p.remove() for p in reversed(ax.patches)]
     poly = patches.Polygon(vertices, edgecolor='r', facecolor="none")
     ax.add_patch(poly)
@@ -50,7 +51,8 @@ def select_vertices(event):
 
 def process_ellipse(x, y):
     global vertices
-    vertices.append([x, y])
+    global angle
+    vertices.append([math.floor(x), math.floor(y)])
     if len(vertices) == 3:
         angle = math.degrees(math.atan((vertices[0][1] - vertices[1][1]) / (vertices[0][0] - vertices[1][0])))
         ellipse = patches.Ellipse(vertices[0], 2 * np.sqrt(np.square(vertices[0][0] - vertices[1][0]) +
@@ -81,7 +83,7 @@ def shape(typ, data, name):
     global fig
     global cid
     fig, ax = plt.subplots(1, num=name + ": Determining Line Regions")
-    ax.imshow(data, cmap='gray', origin='lower')
+    ax.imshow(data, cmap='jet', origin="lower")
     close_ax = plt.axes([0.81, 0.05, 0.1, 0.075])
     close_button = Button(close_ax, 'END')
     close_button.on_clicked(closer)
@@ -94,4 +96,4 @@ def shape(typ, data, name):
               "finished: ")
         cid = fig.canvas.mpl_connect('button_press_event', select_ellipse)
         plt.show()
-    return vertices
+    return vertices, angle

main.py
@@ -1,13 +1,15 @@
 import math
 import os
-import matplotlib.pyplot as plt
+import tkinter as tk
 import matplotlib.patches as patches
+import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image, ImageDraw
 from astropy.io import fits
 from astropy.visualization import astropy_mpl_style
-from prompt import call
-import tkinter as tk
+
 from galaxyShape import shape
+from prompt import call
 
 MAX_ATTEMPTS = 5
 attempts = 1
@@ -245,7 +247,29 @@ def main():
                            "(y/n): ")  # FIXME later
 
     userStr = input("\nElliptical or polygon shape? (e/p): ")
-    shape(userStr, limited_data, win_str)
+    vertices, angle = shape(userStr, limited_data, win_str)
+    vertices = list(map(tuple, vertices))
+    img = Image.new("L", (limited_data.shape[1], limited_data.shape[0]), 0)
+    if userStr == 'e':
+        vertices = vertices[0:3]
+        ImageDraw.Draw(img).ellipse([vertices[0][0] - (np.sqrt(np.square(vertices[0][0] - vertices[1][0]) +
+                                                               np.square(vertices[0][1] - vertices[1][1]))),
+                                     vertices[0][1] - (np.sqrt(np.square(vertices[0][0] - vertices[2][0]) +
+                                                               np.square(vertices[0][1] - vertices[2][1]))),
+                                     vertices[0][0] + (np.sqrt(np.square(vertices[0][0] - vertices[1][0]) +
+                                                               np.square(vertices[0][1] - vertices[1][1]))),
+                                     vertices[0][1] + (np.sqrt(np.square(vertices[0][0] - vertices[2][0]) +
+                                                               np.square(vertices[0][1] - vertices[2][1])))],
+                                    outline=1, fill=1)
+        img = img.rotate(-angle, center=vertices[0])
+        # Pad the array so center of ellipse is center array,
+        # then apply, then trim back to original dimensions
+        mask = np.array(img)
+    else:
+        ImageDraw.Draw(img).polygon(vertices, outline=1, fill=1)
+        mask = np.array(img)
+    plt.imshow(mask, origin='lower')
+    plt.show()
 
 
 if __name__ == '__main__':